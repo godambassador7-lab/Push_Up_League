32d9726aaef065286d0866dd184d9fc0c48ffa40
godambassador7-lab <godambassador7@gmail.com>
2025-10-24 10:44:06 -0400

    wipe

--- sw.js
+++
@@ -1,9 +0,0 @@
-const CACHE='pul-v3-cache';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./firebase.js','./manifest.webmanifest','./assets/icons/icon-192.png','./assets/icons/icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim())});
-self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
-    const copy=resp.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return resp;
-  }).catch(()=>r)));
-});
